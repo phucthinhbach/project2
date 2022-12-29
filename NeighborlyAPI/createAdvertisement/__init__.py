2566c542301d7f0b47e4412adedff4b1be7431f8
Phúc Thịnh <phucthinhbach@gmail.com>
2022-12-29 13:23:02 +0700

    commit 3

--- NeighborlyAPI/createAdvertisement/__init__.py
+++ NeighborlyAPI/createAdvertisement/__init__.py
@@ -7,7 +7,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if request:
         try:
-            url = "mongodb://thinhbnp-accountdb:omSAuHeSulPxEvYScPUWGaim2GqVN41uYF1nL51GsRba7wJ9Jh00j2YOLAkr2i9blIivADlPyyESACDbY6bi9g==@thinhbnp-accountdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@thinhbnp-accountdb@"  # TODO: Update with appropriate MongoDB connection information
+            url = "mongodb://thinhbnp-accountdb:omSAuHeSulPxEvYScPUWGaim2GqVN41uYF1nL51GsRba7wJ9Jh00j2YOLAkr2i9blIivADlPyyESACDbY6bi9g==@thinhbnp-accountdb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@thinhbnp-accountdb@"  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
             database = client['thinhbnp-accountdb']
             collection = database['advertisements']
